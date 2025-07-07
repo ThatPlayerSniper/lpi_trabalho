c6debee40931734e1f111837d2553b8367195775
MCoutinhoB <bcoutinho@ipcbcampus.pt>
2025-07-07 14:35:35 +0100

    define('INCLUDE_CHECK', true);

--- paginas/editar_visu.php
+++ paginas/editar_visu.php
@@ -1,6 +1,9 @@
 <?php
 // Inclui o ficheiro de ligação à base de dados
 require_once "../basedados/basedados.h";
+
+define('INCLUDE_CHECK', true);
+
 // Inclui o ficheiro de autenticação
 require_once "./auth.php";
 // Verifica se já têm uma sessão iniciada caso não tenha cria uma
